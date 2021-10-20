aa71fc876ba049f28cfca153a80e8d27b4275688
严相和 <yanxianghe1992@qq.com>
2021-10-20 10:05:44 +0800

    1.客服端优化

---
+++ app/src/main/java/com/smarthome/magic/activity/shengxian/ShengxianMainActivity.java
@@ -0,0 +1,96 @@
+package com.smarthome.magic.activity.shengxian;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.smarthome.magic.R;
+import com.smarthome.magic.activity.BindBoxActivity;
+import com.smarthome.magic.activity.chelianwang.ScanAddCarActivity;
+import com.smarthome.magic.activity.tongcheng58.TongChengMainActivity;
+import com.smarthome.magic.app.BaseActivity;
+import com.smarthome.magic.config.GlideImageLoader;
+import com.youth.banner.Banner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.Nullable;
+import butterknife.BindView;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class ShengxianMainActivity extends BaseActivity {
+
+
+    @BindView(R.id.banner)
+    Banner banner;
+
+    @Override
+    public int getContentViewResId() {
+        return R.layout.shengxian_act_main;
+    }
+
+    @Override
+    public boolean showToolBar() {
+        return true;
+    }
+
+    @Override
+    protected void initToolbar() {
+        super.initToolbar();
+        tv_title.setText("无人售货柜");
+        iv_rightTitle.setVisibility(View.VISIBLE);
+        iv_rightTitle.setImageResource(R.mipmap.tianjiashebei_icon_sao);
+        tv_title.setTextSize(17);
+        tv_title.setTextColor(getResources().getColor(R.color.black));
+        mToolbar.setNavigationIcon(R.mipmap.backbutton);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        iv_rightTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saoyisao();
+            }
+        });
+    }
+
+    private void saoyisao() {
+        if (EasyPermissions.hasPermissions(this, Manifest.permission.CAMERA)) {
+            ShengxianSaoActivity.actionStart(mContext);
+        } else {
+            EasyPermissions.requestPermissions(this, getString(R.string.xjqx), 0, Manifest.permission.CAMERA);
+        }
+    }
+
+    public static void actionStart(Context context) {
+        Intent intent = new Intent(context, ShengxianMainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initBanner();
+    }
+
+    private void initBanner() {
+        List<Integer> items = new ArrayList<>();
+        items.add(R.mipmap.shengxian_banner1);
+        items.add(R.mipmap.shengxian_banner2);
+
+        //设置图片加载器
+        banner.setImageLoader(new GlideImageLoader());
+        //设置图片集合
+        banner.setImages(items);
+        //banner设置方法全部调用完毕时最后调用
+        banner.start();
+    }
+}
---
+++ app/src/main/java/com/smarthome/magic/activity/shengxian/ShengxianSaoActivity.java
@@ -0,0 +1,227 @@
+package com.smarthome.magic.activity.shengxian;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.gson.Gson;
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.model.Response;
+import com.smarthome.magic.R;
+import com.smarthome.magic.app.BaseActivity;
+import com.smarthome.magic.app.ConstanceValue;
+import com.smarthome.magic.app.Notice;
+import com.smarthome.magic.callback.JsonCallback;
+import com.smarthome.magic.config.AppResponse;
+import com.smarthome.magic.config.UserManager;
+import com.smarthome.magic.dialog.BangdingFailDialog;
+import com.smarthome.magic.dialog.newdia.TishiDialog;
+import com.smarthome.magic.get_net.Urls;
+import com.smarthome.magic.model.CarBrand;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import cn.bingoogolapple.qrcode.core.QRCodeView;
+import cn.bingoogolapple.qrcode.zbar.ZBarView;
+
+public class ShengxianSaoActivity extends BaseActivity implements QRCodeView.Delegate {
+    private static final String tag = ShengxianSaoActivity.class.getSimpleName();
+    @BindView(R.id.zxingview)
+    ZBarView mQRCodeView;
+    @BindView(R.id.capture_flash)
+    ImageView captureFlash;
+
+    private ProgressDialog waitdialog;
+    private boolean flag = true;
+
+    /**
+     * 用于其他Activty跳转到该Activity
+     */
+    public static void actionStart(Context context) {
+        Intent intent = new Intent(context, ShengxianSaoActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initToolbar();
+        mQRCodeView.startSpot();
+        mQRCodeView.setDelegate(this);
+    }
+
+    @Override
+    public int getContentViewResId() {
+        return R.layout.activity_scan1;
+    }
+
+
+    private void light() {
+        if (flag) {
+            flag = false;
+            // 开闪光灯
+            mQRCodeView.openFlashlight();
+            captureFlash.setTag(null);
+            captureFlash.setBackgroundResource(R.drawable.flash_open);
+        } else {
+            flag = true;
+            // 关闪光灯
+            mQRCodeView.closeFlashlight();
+            captureFlash.setTag("1");
+            captureFlash.setBackgroundResource(R.drawable.flash_default);
+        }
+    }
+
+
+    private void vibrate() {
+        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        vibrator.vibrate(200);
+    }
+
+    @Override
+    public void onScanQRCodeSuccess(String result) {
+        waitdialog = ProgressDialog.show(ShengxianSaoActivity.this, null, "已扫描，正在处理···", true, true);
+        waitdialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            public void onDismiss(DialogInterface dialog) {
+                mQRCodeView.stopSpot();
+            }
+        });
+
+        vibrate();
+
+        waitdialog.dismiss();
+
+        saoma(result);
+    }
+
+    private void showTishi(String msg) {
+        TishiDialog dialog = new TishiDialog(mContext, TishiDialog.TYPE_XIAOXI, new TishiDialog.TishiDialogListener() {
+            @Override
+            public void onClickCancel(View v, TishiDialog dialog) {
+
+            }
+
+            @Override
+            public void onClickConfirm(View v, TishiDialog dialog) {
+
+            }
+
+            @Override
+            public void onDismiss(TishiDialog dialog) {
+
+            }
+        });
+        dialog.setTextConfirm("确定");
+        dialog.setTextCancel("");
+        dialog.setTextContent(msg);
+        dialog.show();
+    }
+
+    public void saoma(String device_ccid) {
+        Map<String, String> map = new HashMap<>();
+        map.put("code", "100026");//正式的
+        map.put("key", Urls.key);
+//        map.put("token", UserManager.getManager(mContext).getAppToken());
+        map.put("wx_token", "1111111111");
+        map.put("line_type", "1");
+        map.put("device_ccid", device_ccid);
+
+        Gson gson = new Gson();
+        OkGo.<AppResponse<CarBrand.DataBean>>post(Urls.SERVER_URL + "cs/app/user")
+                .tag(this)//
+                .upJson(gson.toJson(map))
+                .execute(new JsonCallback<AppResponse<CarBrand.DataBean>>() {
+                    @Override
+                    public void onSuccess(final Response<AppResponse<CarBrand.DataBean>> response) {
+                        TishiDialog dialog = new TishiDialog(mContext, TishiDialog.TYPE_SUCESS, new TishiDialog.TishiDialogListener() {
+                            @Override
+                            public void onClickCancel(View v, TishiDialog dialog) {
+
+                            }
+
+                            @Override
+                            public void onClickConfirm(View v, TishiDialog dialog) {
+
+                            }
+
+                            @Override
+                            public void onDismiss(TishiDialog dialog) {
+
+                            }
+                        });
+                        dialog.setTextConfirm("确定");
+                        dialog.setTextCancel("");
+                        dialog.setTextContent("扫码成功");
+                        dialog.show();
+                    }
+
+                    @Override
+                    public void onError(Response<AppResponse<CarBrand.DataBean>> response) {
+                        String msg = response.getException().getMessage();
+                        showTishi(msg);
+                    }
+                });
+    }
+
+
+    @Override
+    public void onScanQRCodeOpenCameraError() {
+        Log.e(tag, "打开相机出错");
+        mQRCodeView.startCamera();
+    }
+
+    @OnClick({R.id.capture_flash})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.capture_flash:
+                light();
+                break;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mQRCodeView.stopCamera();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (waitdialog != null) {
+            waitdialog.dismiss();
+        }
+    }
+
+    @Override
+    protected void initToolbar() {
+        super.initToolbar();
+        tv_title.setText("扫一扫");
+        tv_title.setTextSize(17);
+        tv_title.setTextColor(getResources().getColor(R.color.black));
+        mToolbar.setNavigationIcon(R.mipmap.backbutton);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean showToolBar() {
+        return true;
+    }
+
+}
